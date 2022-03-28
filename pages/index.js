08c750bdb7c24733635aa94763d6123939c4984d
Manoj <manojkumarmondal554@gmail.com>
2022-03-28 20:34:40 +0530

    new feature: about,service

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Home &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>lorem fgbidfub kjdfgvbbbdf</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
---
+++ pages/service.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const service = () => {
+    return (
+        <div>
+            <div>This is Service Page</div>
+            <h1>HEading</h1>
+        </div>
+    )
+}
+
+export default service
