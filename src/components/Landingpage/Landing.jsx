98fb8af59ab4cc15d7b108776d170e988fb2eff3
Tamal <tkdas.kisan@protonmail.com>
2022-05-18 10:22:19 +0530

    arrow added to landing page button and google sign up removed

--- src/components/Landingpage/Landing.jsx
+++ src/components/Landingpage/Landing.jsx
@@ -18,11 +18,11 @@ const Landing = () => {
                 <div className={styles.btn}>
                     {auth.authenticate ? (
                         <Link href={'/products'}>
-                            <button className={styles.btngreen}>Go to add products {'>>'}</button>
+                            <button className={styles.btngreen}>Go to add products &#x2794;</button>
                         </Link>
                     ):(
                         <Link href={'/signin'}>
-                        <button className={styles.btnred}>Begin your journey with Cartany! {'>>'}</button>
+                        <button className={styles.btnred}>Begin your journey with Cartany! &#x2794;</button>
                         </Link>
 
                     ) }
